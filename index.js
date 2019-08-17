0150d86ec7c24a9ce93441f51f252b2bec3d1e73
Aubrey Rhodes <aubrey.c.rhodes@gmail.com>
2019-08-17 10:47:19 -0400

    Update app for heroku
    Co-authored-by: JOHNSOV5 <johnsonbpo@yahoo.com>
    Co-authored-by: microagent <aklein@osisecure.com>
    Co-authored-by: Alexander Santos <alexsantosantana@live.com>
    Co-authored-by: elizabethreese <reeseelizabeth25@gmail.com>
    Co-authored-by: ReenaGo <reenagouldbourne@gmail.com>
    Co-authored-by: Matthew Cuin <mattheew1@gmail.com>

--- index.js
+++ index.js
@@ -152,6 +152,8 @@ app.get("/likeMessage/:id", function(req, res, next){
   });
 })
 
-app.listen(3000, function () {
-  console.log("listening on port 3000...");
+var port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+  console.log(`listening on port ${ port }...`);
 })
